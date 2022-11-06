d983135396029fb2921075801c5b0a1dcdbe3ee0
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-11-06 11:31:49 -0300

    Consumir datos de db en el front

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,6 @@
 <?php
 
-function obtenerServicios()
+function obtenerServicios(): array
 {
     try {
         //importanr conexión
@@ -11,13 +11,32 @@ function obtenerServicios()
         $sql = "SELECT * FROM servicios;";
         $consulta = mysqli_query($db, $sql); //dos parámetros, la db y la query a realizar
 
+        //Array vacío
+
+        $servicios = [];
+        $i = 0;
+
         //obtener resultados
 
-        echo "<pre>";
+        /* echo "<pre>";
         var_dump(mysqli_fetch_assoc($consulta)); //mysqli_fetch_assoc convierte los resultados a un array de php
         // var_dump(mysqli_fetch_all($consulta)); all trae todos los resultados
-        echo "</pre>";
-        //
+        echo "</pre>"; */
+
+        while ($row = mysqli_fetch_assoc(($consulta))) {
+            $servicios[$i]['id'] = $row['id'];
+            $servicios[$i]['nombre'] = $row['nombre'];
+            $servicios[$i]['precio'] = $row['precio'];
+
+            $i++;
+        }
+
+        /* echo "<pre>";
+        var_dump(json_encode($servicios));
+        echo "</pre>"; */
+
+        return $servicios;
+
         //
     } catch (\Throwable $th) {
         //throw $th;
